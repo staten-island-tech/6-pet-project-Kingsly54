8d3d512f8ad728ecfb97872a0e7f035c4a75c4ce
Kingsly54 <kingslyw@nycstudents.net>
2026-04-21 09:26:07 -0400

    h

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 ###Make your own hero object with a name, starting money, and one item.
 
 ###Then, use `.buy()` to add another item to your inventory.
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -13,16 +13,26 @@ class Hero:
         self.inventory.append(item)
         print(self.inventory)
 Johnny=Hero("Johnny", 67, ["Potion"])
-Johnny.buy("Sword")
-
-
-
-
-
+Johnny.buy("Sword") """
+#✏️ Activity 2 – Add Encapsulation
+#Create a class Pet that has:
+#A name
+#A private variable for happiness level (e.g., __happiness)
+#A method to play() that increases happiness
+#A method to show_status() that prints how happy the pet is
 class Pet:
     def __init__(self, name, happiness, hunger):
         self.name=name
         self.happiness=happiness
         self.hunger=hunger
-    def play(self,play)
-Daniel=Pet("Daniel", 0, 0)
+    def play(self,timeplay):
+        hgain=timeplay*.5
+        hloss=timeplay*.1
+        self.happiness+=hgain
+        self.hunger-=hloss
+    def checkstat(self):
+        print(self.happiness)
+        print(self.hunger)
+Daniel=Pet("Daniel", 0, 0) 
+Daniel.play(4)
+Daniel.checkstat
